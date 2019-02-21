952decaa416f05dcb92472dda9c0ef3bbebdf4b7
Nepein Andrey <nepein96@gmail.com>
2019-02-22 00:00:59 +0300

    Updates

---
+++ precache-manifest.d4d4d17257f2e6fce21d8aed980bfc6f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "252f084c38befba002ef4bf96c9ca376",
+    "url": "/frontend/build/static/media/sweat.252f084c.svg"
+  },
+  {
+    "revision": "76d3b037df333593810a",
+    "url": "/frontend/build/static/css/main.75bc71fd.chunk.css"
+  },
+  {
+    "revision": "7a5e162d783af055a9fdda3d87ba354a",
+    "url": "/frontend/build/static/media/test.7a5e162d.svg"
+  },
+  {
+    "revision": "69ffd9a920cca20c46d3",
+    "url": "/frontend/build/static/js/1.69ffd9a9.chunk.js"
+  },
+  {
+    "revision": "589be86112ae31ac2b6a",
+    "url": "/frontend/build/static/js/runtime~main.589be861.js"
+  },
+  {
+    "revision": "5d1604d7aef5c56ef59aa0e3a22a910b",
+    "url": "/frontend/build/static/media/feathers.5d1604d7.svg"
+  },
+  {
+    "revision": "12ac7e5e6813c5703e08a8bed327fd18",
+    "url": "/frontend/build/static/media/reflection.12ac7e5e.svg"
+  },
+  {
+    "revision": "76d3b037df333593810a",
+    "url": "/frontend/build/static/js/main.76d3b037.chunk.js"
+  },
+  {
+    "revision": "3270f2832585b8a099726dd12c9e417a",
+    "url": "/frontend/build/static/media/layers.3270f283.svg"
+  },
+  {
+    "revision": "8eb825b94dfb14a5693b6db9f255c722",
+    "url": "/frontend/build/static/media/graph.8eb825b9.svg"
+  },
+  {
+    "revision": "b5dde3a02689437ea47e75a508eab45a",
+    "url": "/frontend/build/static/media/list.b5dde3a0.svg"
+  },
+  {
+    "revision": "47590da81dd308d08c70c71aea112046",
+    "url": "/frontend/build/static/media/log-in.47590da8.svg"
+  },
+  {
+    "revision": "69ffd9a920cca20c46d3",
+    "url": "/frontend/build/static/css/1.f7f1b9bc.chunk.css"
+  },
+  {
+    "revision": "0f7128eab9f89d20b664e0bd4a447590",
+    "url": "/frontend/build/index.html"
+  }
+];
