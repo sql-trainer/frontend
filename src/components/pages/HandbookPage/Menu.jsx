10e99eda230af522215c0f4d65f921b047b750a9
Ladanov Nikolay <ladanov_n@mail.ru>
2019-05-07 23:48:48 +0300

    added menu for handbook page

--- src/components/pages/HandbookPage/Menu.jsx
+++ src/components/pages/HandbookPage/Menu.jsx
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
 
 class MenuHandbookPage extends Component {
-  render() {
-    return <div className="handbook__menu"></div>;
+	
+	componentDidMount() {
+		this.setState({
+			opened: true
+		})
+	}
+
+  	render() {
+    	return (
+      	<div className="handbook__menu">
+			<div className="toggle">
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+			<div className="content">
+				<div className="nav">
+					{/* <a href="#">Введение в SQL</a>
+					<a href="#">Введение в SQL</a> */}
+				</div>
+			</div>
+      	</div>);
   }
 }
 
