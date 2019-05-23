c1785e95f77566cd41d7eeeffa2388c89f60f6cd
Nepein Andrey <nepein96@gmail.com>
2019-05-23 18:45:01 +0300

    Jenkins test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './components/App';
 import persistedStore from './store/';
-
+console.log('test');
 const { store, persistor } = persistedStore();
 
 ReactDOM.render(
